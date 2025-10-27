bc2e5f39d908aa1bbdae82f313c54958226f9e11
tarak6984 <tareksabbir20@gmail.com>
2025-10-27 14:01:51 +0600

    Fix all Clerk imports - client vs server

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,4 @@
-import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs/server";
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
