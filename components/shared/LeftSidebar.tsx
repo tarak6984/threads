df21b1fdf5ff5c22b98502571139c93dfe705398
tarak6984 <tareksabbir20@gmail.com>
2025-10-27 14:13:36 +0600

    Fix SignOutButton deprecated prop for Clerk v5

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -44,7 +44,7 @@ const LeftSidebar = () => {
 
       <div className='mt-10 px-6'>
         <SignedIn>
-          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+          <SignOutButton redirectUrl="/sign-in">
             <div className='flex cursor-pointer gap-4 p-4'>
               <Image
                 src='/assets/logout.svg'
